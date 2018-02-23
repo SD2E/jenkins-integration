b52edcaea29746ad7a845eb4be3b8227dbb20c03
Tom Mitchell <tcmitchell@users.noreply.github.com>
2018-02-23 16:23:48 -0500

    xplan problem moved to s_expr

--- xplan-rule30-end-to-end-demo.py
+++ xplan-rule30-end-to-end-demo.py
@@ -3,7 +3,7 @@ from agavepy.agave import Agave
 import json
 import time
 import xplan_api.lab.transcriptic as transcriptic_config
-import xplan_api.problem as prob
+import xplan_api.s_expr.problem as prob
 from pyDOE import *
 
 exit(0)
