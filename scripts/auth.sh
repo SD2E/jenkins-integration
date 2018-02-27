015c70387e1ceab6424da47ab150163accdd452d
mwes <mark.william.weston@gmail.com>
2018-02-27 16:39:45 -0500

    Update auth.sh

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -10,3 +10,6 @@ echo $client > client.bak
 clients-create -S -N $client -D "My client used for interacting with SD2E" -u $AGAVE_USER -p $AGAVE_PASSWORD
 auth-tokens-create -S -p $AGAVE_PASSWORD
 auth-check
+
+echo $AGAVE_USER > user_post_build.props
+echo $AGAVE_PASSWORD > pw_post_build.props
