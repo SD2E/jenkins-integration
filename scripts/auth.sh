6fa98dfd186dc321eb2cf2dba0568ed8f4a85eca
mwes <mark.william.weston@gmail.com>
2018-02-27 16:38:55 -0500

    Update auth.sh

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -3,16 +3,6 @@ curl -L https://raw.githubusercontent.com/sd2e/sd2e-cli/master/install/install.s
 source ~/.bashrc
 tenants-init -t sd2e
 
-if [ -f "client.bak" ]
-then
-  echo "Reading Client"
-  myclient=$(cat "client.bak")
-  echo $myclient
-  clients-delete -u $AGAVE_USER -p $AGAVE_PASSWORD $myclient
-fi
-
-rm -f client.bak
-
 client="sd2e_client_$BUILD_TAG"
 echo "Writing Client"
 echo $client > client.bak
