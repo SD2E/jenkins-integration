72f665b8eaab5de95a7806de48afd77cff0d5fd0
Tom Mitchell <tcmitchell@users.noreply.github.com>
2018-03-01 10:40:16 -0500

    list files

--- init-sd2e.sh
+++ init-sd2e.sh
@@ -5,6 +5,8 @@
 
 set -x
 
+tenants-init -t sd2e
+
 client="sd2e_client_$BUILD_TAG"
 echo "Writing Client"
 echo $client > client.bak
@@ -12,3 +14,5 @@ echo $client > client.bak
 clients-create -S -N $client -D "My client used for interacting with SD2E" -u $AGAVE_USER -p $AGAVE_PASSWORD
 auth-tokens-create -S -p $AGAVE_PASSWORD
 auth-check
+
+files-list
